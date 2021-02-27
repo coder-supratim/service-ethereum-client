b43235778ef5159a8de07e63e6422f67e939353d
coder-supratim <supratim.arya@gmail.com>
2021-02-27 16:58:41 -0500

    Updated java doc

--- src/test/java/com/app/ace/dao/DataConnectorTest.java
+++ src/test/java/com/app/ace/dao/DataConnectorTest.java
@@ -7,7 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 /**
- * Created by Dmitry Bolotin on 2/25/21.
+ *
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = {EthereumClientApplication.class})
