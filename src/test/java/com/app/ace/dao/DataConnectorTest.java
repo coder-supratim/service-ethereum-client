4d1eb3c2682bbb1004c474287dfc3923cdee00fb
coder-supratim <supratim.arya@gmail.com>
2021-02-27 16:17:43 -0500

    Updated service names

--- src/test/java/com/app/ace/dao/DataConnectorTest.java
+++ src/test/java/com/app/ace/dao/DataConnectorTest.java
@@ -1,21 +1,16 @@
 package com.app.ace.dao;
 
-import com.app.ace.app.AccessibilityCoinExchangeApplication;
-import com.app.ace.controller.AceController;
-import org.junit.jupiter.api.Test;
+import com.app.ace.app.EthereumClientApplication;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Created by Dmitry Bolotin on 2/25/21.
  */
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = {AccessibilityCoinExchangeApplication.class})
+@SpringBootTest(classes = {EthereumClientApplication.class})
 class DataConnectorTest {
 
     @Autowired
